65c5f0ff881197cece3d7ecc0bc9cf7a13b80ba2
Praveena-Krishnan <praveenakrishnan2003@gmail.com>
2024-11-26 13:53:31 +0530

    Profile card without map

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import Contact from "./Pages/Contact";
 import Products from "./Pages/Products";
 import { Routes,Route } from "react-router-dom";
 import Student from "./student";
+import ProfileCard from "./Components/ProfileCard";
 function App(){
   return (
-  <div><Message/>
+  <div>
+  <Message/>
   <>
     <Student name ="John" age={23} />
     <Student name ="Riya" age={22} />
@@ -21,6 +23,12 @@ function App(){
     <Route path='/about' element={<About/>}/>
     <Route path='/contact' element={<Contact/>}/>
  </Routes>
+ <div>
+      <h1>Profile Cards</h1>
+      <ProfileCard name="Alice" age={25} image="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg" />
+      <ProfileCard name="Bob" age={30} image="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg" />
+      <ProfileCard name="Charlie" age={20} image="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg" />
+    </div>
 
     
     </div> 
